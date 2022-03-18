99435472400a70aa470db8ba2372326a8096b076
Jason S Fillman <github@jasonfillman.com>
2022-03-17 22:14:24 -0700

    Testing combined pr build script

--- .github/workflows/scripts/pr_build.sh
+++ .github/workflows/scripts/pr_build.sh
@@ -1,12 +1,21 @@
 #! /usr/bin/env bash
 
 export MY_ORG=$(echo "${GITHUB_REPOSITORY}" | awk -F / '{print $1}')
-export MY_APP=$(echo $(echo "${GITHUB_REPOSITORY}" | awk -F / '{print $2}')"-develop")
 export DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 export BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 export COMMIT=$(echo "$SHA" | cut -c -7)
 
 echo $GITHUB_BASE_REF
+export MY_APP=$(echo $(echo "${GITHUB_REPOSITORY}" | awk -F / '{print $2}')"-develop")
+
+if $GITHUB_BASE_REF = "develop"; then
+  export MY_APP=$(echo $(echo "${GITHUB_REPOSITORY}" | awk -F / '{print $2}')"-develop")
+elif $GITHUB_BASE_REF = "main" || $GITHUB_BASE_REF = "master"; then
+  export MY_APP=$(echo "${GITHUB_REPOSITORY}" | awk -F / '{print $2}'
+else
+  echo "Target branch must be develop, main, or master";
+  exit 1
+fi
 
 echo $DOCKER_TOKEN | docker login ghcr.io -u $DOCKER_ACTOR --password-stdin
 docker build --build-arg BUILD_DATE="$DATE" \
@@ -16,4 +25,3 @@ docker build --build-arg BUILD_DATE="$DATE" \
              --label us.kbase.vcs-pull-req="$PR" \
              -t ghcr.io/"$MY_ORG"/"$MY_APP":"pr-""$PR" .
 docker push ghcr.io/"$MY_ORG"/"$MY_APP":"pr-""$PR"
-	
